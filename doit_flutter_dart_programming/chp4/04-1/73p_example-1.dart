30bbbf2617a13206713c2a0fa3dabd8d25af5727
gkstmdgnsgong <gkstmdgnsgong@gmail.com>
2025-04-30 15:05:08 +0900

    feat:73p 예제

---
+++ doit_flutter_dart_programming/chp4/04-1/73p_example-1.dart
@@ -0,0 +1,13 @@
+//문자열 표현법
+main() {
+  String data1 = 'hello';
+  String data2 = "world";
+  String data3 = '''
+    hello
+    world
+  '''; // 3개의 따옴표를 사용하여 여러 줄을 표현할 수 있다.
+  String data4 = """
+    hello
+    world
+  """;
+}
---
+++ doit_flutter_dart_programming/chp4/04-1/73p_example-2.dart
@@ -0,0 +1,7 @@
+//문자열 비교
+main() {
+  String str1 = 'hello';
+  String str2 = 'hello';
+
+  print(str1 == str2); //true
+}
