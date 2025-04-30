f6d3f40f52a69c5d558e120ae0b200cd366fb1f1
gkstmdgnsgong <gkstmdgnsgong@gmail.com>
2025-04-30 15:57:52 +0900

    feat: 76,77-1 complete

--- doit_flutter_dart_programming/chp4/04-1/76p_example-1.dart
+++ doit_flutter_dart_programming/chp4/04-1/76p_example-1.dart
@@ -1,8 +1,15 @@
-// int와 String 형 변환
-main() {
-  int n1 = 10;
-  String s1 = '10';
+// 초깃값 없이 const 변수 선언 오류
 
-  String s2 = n1.toString(); // int -> String 성공
-  int n2 = int.parse(s1); // String -> int 성공, parse(파서) : 분석하다
+/*
+  다음 코드는 const 변수를 선언할 때 초깃값을 대입하지 않아서 오류 발생
+*/
+
+// const String data1; // 오류
+
+class User {
+  // static const String data2; // 오류
+
+  void some() {
+    // const String data3; // 오류
+  }
 }
--- doit_flutter_dart_programming/chp4/04-1/77p_example-1.dart
+++ doit_flutter_dart_programming/chp4/04-1/77p_example-1.dart
@@ -1,8 +1,20 @@
-// int와 String 형 변환
-main() {
-  int n1 = 10;
-  String s1 = '10';
+// 상수 변숫값 바꾸기 오류
 
-  String s2 = n1.toString(); // int -> String 성공
-  int n2 = int.parse(s1); // String -> int 성공, parse(파서) : 분석하다
+/*
+  다음 코드는 const 변수를 선언하면서 초깃값을 대입해 오류가 발생하지는 않지만
+  초깃값을 바꿀 수 없는 상수 변수에서 다시 값을 대입한 부분에서 오류가 발생
+*/
+
+const String data1 = 'hello';
+
+class User {
+  static const String data2 = 'hello';
+
+  void some() {
+    const String data3 = 'hello';
+
+    // data1 = 'world'; // 오류
+    // data2 = 'world'; // 오류
+    // data3 = 'world'; // 오류
+  }
 }
