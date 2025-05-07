ae992821e2560a98e23daf4ff6497313823f6e2c
gkstmdgnsgong <gkstmdgnsgong@gmail.com>
2025-05-07 18:00:57 +0900

    feat: 165p_example-1.dart complete

---
+++ doit_flutter_dart_programming/chp8/08-4/165p_example-1.dart
@@ -0,0 +1,18 @@
+// //상태 클래스
+
+// // mac: 'command + /' 로 주석을 해제해서 사용하세요
+// //chp8 부터는 플러터 프로젝트 main.dart 파일에 복사, 붙여넣기로 실행 요망
+
+// /*
+// StatefulWidget은 createState() 메서드를 반드시 작성해야 함
+// createState() 함수에서 State 객체를 반환
+// 하지만 createState()는 StatefulWidget이 처음 생성되는 순간에만 호출됨
+// 즉, 화면이 다시 빌드되어도 매번 호출되지 않음
+// */
+
+// class MySubStatefulWidget extends StatefulWidget {
+//   @override
+//   State<StatefulWidget> createState() {
+//     return _MySubStatefulWidgetState();
+//   }
+// }
