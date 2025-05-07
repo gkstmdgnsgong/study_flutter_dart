a3070267a364b7031030fe8f2eb2f56840e4beea
gkstmdgnsgong <gkstmdgnsgong@gmail.com>
2025-05-07 09:40:29 +0900

    feat: 151p_example-2.dart, complete

--- doit_flutter_dart_programming/chp8/08-1/151p_example-1.dart
+++ doit_flutter_dart_programming/chp8/08-1/151p_example-1.dart
@@ -1,10 +1,19 @@
-//runApp() 함수로 루트 위젯 등록하기
-// 교재와 다르게 MaterialApp() 을 사용해야 실행됨
+// //runApp() 함수로 루트 위젯 등록하기
+// // 교재와 다르게 MaterialApp() 을 사용해야 실행됨
 
-//chp8 부터는 플러터 프로젝트 main.dart 파일에 복사, 붙여넣기로 실행 요망
+// //chp8 부터는 플러터 프로젝트 main.dart 파일에 복사, 붙여넣기로 실행 요망
 
-import 'package:flutter/material.dart';
+// /*
+// runApp() 함수에 Center(플러터 기본 제공, 가운데 정렬을 나타내는 위젯) 위젯 전달
+// Center 위젯에 Text(텍스트 나타내는) 위젯을 포함
 
-void main() {
-  runApp(MaterialApp(home: Center(child: Text('Hello'))));
-}
+// runApp() 함수는 전달받은 위젯을 위젯 트리의 루트로 만듭니다
+// 그리고 플러터 프레임 워크는 이 루트 위젯으로 화면을 덮음
+
+// 플러터 앱의 화면은 이처럼 필요한 위젯을 계층으로 나열해서 구성
+// */
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(MaterialApp(home: Center(child: Text('Hello'))));
+// }
--- doit_flutter_dart_programming/flutter_application_doitflutter/lib/main.dart
+++ doit_flutter_dart_programming/flutter_application_doitflutter/lib/main.dart
@@ -1,7 +1,34 @@
-//runApp() 함수로 루트 위젯 등록하기
+//화면 구성 예
+//chp8 부터는 플러터 프로젝트 main.dart 파일에 복사, 붙여넣기로 실행 요망
 
+/*
+Myapp 클래스 : 앱 화면 전체 구성 위젯, StatelessWidget 상속
+  (08-3 상세 설명하겠지만 StatelessWidget을 상속 받은 위젯은 화면에 보일 뷰를 갱신할 수 없음 즉, 정적인 화면)
+
+위젯 클래스 : 주요 작업은 다른 위젯을 계층으로 조합해 build() 함수를 구현
+  예제에서는 7개 위젯을 계층 조합하여 MyApp 클래스의 build() 함수 구현
+  - MaterialApp : 머티리얼 디자인 적용
+  - Scaffold : 화면 구조 설계
+  - AppBar : 화면 위쪽 앱바 구성
+  - Text : 앱바 제목
+  - Center : 가운데 정렬
+  - GestureDetector : 사용자 이벤트 처리
+  - Text : 본문에 문자열 출력
+*/
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Center(child: Text('Hello'))));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Test')),
+        body: Center(child: GestureDetector(child: Text('Hello, World!'))),
+      ),
+    );
+  }
 }
