7ab22a1a4161fcc7080566b14a348cf32aab1a99
gkstmdgnsgong <gkstmdgnsgong@gmail.com>
2025-05-07 09:17:40 +0900

    feat: 151p_example-1.dart complete

--- doit_flutter_dart_programming/chp8/08-1/151p_example-1.dart
+++ doit_flutter_dart_programming/chp8/08-1/151p_example-1.dart
@@ -1,4 +1,7 @@
 //runApp() 함수로 루트 위젯 등록하기
+// 교재와 다르게 MaterialApp() 을 사용해야 실행됨
+
+//chp8 부터는 플러터 프로젝트 main.dart 파일에 복사, 붙여넣기로 실행 요망
 
 import 'package:flutter/material.dart';
 
--- doit_flutter_dart_programming/flutter_application_doitflutter/lib/chp8/08-1/151p_example-1.dart
+++
@@ -1,3 +0,0 @@
-//runApp() 함수로 루트 위젯 등록하기
-
-import 'package:flutter/material.dart';
