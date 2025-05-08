e3f80d04019f75abe1329e2420d16528819f2044
gkstmdgnsgong <gkstmdgnsgong@gmail.com>
2025-05-08 13:26:20 +0900

    feat: 173p_example-2.dart complete

--- doit_flutter_dart_programming/chp8/08-6/173p_example-1.dart
+++ doit_flutter_dart_programming/chp8/08-6/173p_example-1.dart
@@ -1,10 +1,10 @@
-//상위 위젯 객체 얻기
+// //상위 위젯 객체 얻기
 
-// mac: 'command + /' 로 주석을 해제해서 사용하세요
-//chp8 부터는 플러터 프로젝트 main.dart 파일에 복사, 붙여넣기로 실행 요망
+// // mac: 'command + /' 로 주석을 해제해서 사용하세요
+// //chp8 부터는 플러터 프로젝트 main.dart 파일에 복사, 붙여넣기로 실행 요망
 
-/*
-상위에서 MyApp 위젯의 객체를 얻는 코드
-어디엔가 위젯의 정보가 유지되므로 이런 코드가 가능하다
-*/
-MyApp? app = context.findAncestorWidgetOfExactType<Myapp>();
+// /*
+// 상위에서 MyApp 위젯의 객체를 얻는 코드
+// 어디엔가 위젯의 정보가 유지되므로 이런 코드가 가능하다
+// */
+// MyApp? app = context.findAncestorWidgetOfExactType<Myapp>();
---
+++ doit_flutter_dart_programming/chp8/08-6/173p_example-2.dart
@@ -0,0 +1,9 @@
+// //Element 선언문
+
+// // mac: 'command + /' 로 주석을 해제해서 사용하세요
+// //chp8 부터는 플러터 프로젝트 main.dart 파일에 복사, 붙여넣기로 실행 요망
+
+// /*
+// 엘리먼트 트리를 구성하는 Component Element, RenderObjectElement의 상위 타입이 Element인데 다음처럼 선언됨
+// */
+// abstract class Element extends DiagnosticableTree implements BuildContext {
