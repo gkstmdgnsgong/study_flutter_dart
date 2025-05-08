f5520672e0ae5daba8494e0ff4639471aaa0c6d2
gkstmdgnsgong <gkstmdgnsgong@gmail.com>
2025-05-08 10:48:36 +0900

    feat: 170p_example-1.dart complete

---
+++ doit_flutter_dart_programming/chp8/08-5/170p_example-1.dart
@@ -0,0 +1,36 @@
+// //State의 생명 주기 함수 호출 시점
+
+// // mac: 'command + /' 로 주석을 해제해서 사용하세요
+// //chp8 부터는 플러터 프로젝트 main.dart 파일에 복사, 붙여넣기로 실행 요망
+
+// /*
+// */
+
+// import 'package:flutter/widgets.dart';
+
+// class _ChildWidgetState extends State<ChildWidget> {
+//   @override
+//   void initState() {
+//     super.initState();
+//   }
+
+//   @override
+//   void didChangeDependencies() {
+//     super.didChangeDependencies();
+//   }
+
+//   @override
+//   void dispose() {
+//     super.dispose();
+//   }
+
+//   @override
+//   void didUpdateWidget(ChildWidget oldWidget) {
+//     super.didUpdateWidget(oldWidget);
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     // 생략...
+//   }
+// }
--- doit_flutter_dart_programming/flutter_application_doitflutter/lib/main.dart
+++ doit_flutter_dart_programming/flutter_application_doitflutter/lib/main.dart
@@ -1,4 +1,4 @@
-//lib/ch8_4_stateful/test.dart
+//State의 생명 주기 함수 호출 시점
 
 // mac: 'command + /' 로 주석을 해제해서 사용하세요
 //chp8 부터는 플러터 프로젝트 main.dart 파일에 복사, 붙여넣기로 실행 요망
@@ -6,68 +6,31 @@
 /*
 */
 
-import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
+class _ChildWidgetState extends State<ChildWidget> {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text('Stateful Test')),
-        body: MyWidget(),
-      ),
-    );
+  void initState() {
+    super.initState();
   }
-}
 
-class MyWidget extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() => _MyWidgetState();
-}
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+  }
 
-class _MyWidgetState extends State<MyWidget> {
-  bool enabled = false;
-  String stateText = "disable";
+  @override
+  void dispose() {
+    super.dispose();
+  }
 
-  void changeCheck() {
-    setState(() {
-      if (enabled) {
-        stateText = "disable";
-        enabled = false;
-      } else {
-        stateText = "enable";
-        enabled = true;
-      }
-    });
+  @override
+  void didUpdateWidget(ChildWidget oldWidget) {
+    super.didUpdateWidget(oldWidget);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          IconButton(
-            icon:
-                (enabled
-                    ? Icon(Icons.check_box, size: 20)
-                    : Icon(Icons.check_box_outline_blank, size: 20)),
-            color: Colors.red,
-            onPressed: changeCheck,
-          ),
-          Container(
-            padding: EdgeInsets.only(left: 10),
-            child: Text(
-              '$stateText',
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-            ),
-          ),
-        ],
-      ),
-    );
+    // 생략...
   }
 }
