90c2eff558d1b157ba301e8b4881d05857369dbe
gkstmdgnsgong <gkstmdgnsgong@gmail.com>
2025-05-08 10:08:49 +0900

    feat: 167p_example-1.dart complete

--- doit_flutter_dart_programming/chp8/08-4/167p_example-1.dart
+++ doit_flutter_dart_programming/chp8/08-4/167p_example-1.dart
@@ -1,9 +1,73 @@
-//lib/ch8_4_stateful/test.dart
+// //lib/ch8_4_stateful/test.dart
 
-// mac: 'command + /' 로 주석을 해제해서 사용하세요
-//chp8 부터는 플러터 프로젝트 main.dart 파일에 복사, 붙여넣기로 실행 요망
+// // mac: 'command + /' 로 주석을 해제해서 사용하세요
+// //chp8 부터는 플러터 프로젝트 main.dart 파일에 복사, 붙여넣기로 실행 요망
 
-/*
-*/
+// /*
+// */
 
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(title: Text('Stateful Test')),
+//         body: MyWidget(),
+//       ),
+//     );
+//   }
+// }
+
+// class MyWidget extends StatefulWidget {
+//   @override
+//   State<StatefulWidget> createState() => _MyWidgetState();
+// }
+
+// class _MyWidgetState extends State<MyWidget> {
+//   bool enabled = false;
+//   String stateText = "disable";
+
+//   void changeCheck() {
+//     setState(() {
+//       if (enabled) {
+//         stateText = "disable";
+//         enabled = false;
+//       } else {
+//         stateText = "enable";
+//         enabled = true;
+//       }
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Center(
+//       child: Row(
+//         mainAxisAlignment: MainAxisAlignment.center,
+//         children: [
+//           IconButton(
+//             icon:
+//                 (enabled
+//                     ? Icon(Icons.check_box, size: 20)
+//                     : Icon(Icons.check_box_outline_blank, size: 20)),
+//             color: Colors.red,
+//             onPressed: changeCheck,
+//           ),
+//           Container(
+//             padding: EdgeInsets.only(left: 10),
+//             child: Text(
+//               '$stateText',
+//               style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
