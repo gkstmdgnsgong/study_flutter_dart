b1ec39fa0740855475f16292d24310e15655736f
gkstmdgnsgong <gkstmdgnsgong@gmail.com>
2025-05-08 14:00:02 +0900

    feat: 174p_example-1.dart complete

---
+++ doit_flutter_dart_programming/chp8/08-6/174p_example-1.dart
@@ -0,0 +1,9 @@
+// //Text 위젯 생성자
+
+// // mac: 'command + /' 로 주석을 해제해서 사용하세요
+// //chp8 부터는 플러터 프로젝트 main.dart 파일에 복사, 붙여넣기로 실행 요망
+
+// /*
+// */
+
+// const Text(String data,{Key? key, //...생략 })
